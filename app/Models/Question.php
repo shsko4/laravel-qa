8d68dd41729e78720063069100ce6c18814fe839
Hussain Alhag <shsko4@gmail.com>
2022-06-15 20:24:39 +0200

    factories and seeds

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -22,7 +22,7 @@ class Question extends Model
     public function setTitleAttribute($value){
 
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value, '-');
+        $this->attributes['slug'] = Str::slug($value, '-');
     }
 
 
