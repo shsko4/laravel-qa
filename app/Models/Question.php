d41ebf5af52831815445c8ec547c3935fb17adfa
Hussain Alhag <shsko4@gmail.com>
2022-06-15 03:16:41 +0200

    Generate Question Model

---
+++ app/Models/Question.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillabl = ['title','body'];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function setTitleAttribute($value){
+
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+
+
+}
