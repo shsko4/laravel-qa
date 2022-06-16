5ed8654d7413d797807d725a887a036ff3e9da0a
Hussain Alhag <shsko4@gmail.com>
2022-06-17 00:03:17 +0200

    add votes and veiws

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -36,5 +36,25 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0){
+
+            if($this->best_answer_id){
+
+                return 'answer-accepted';
+
+            }
+
+            return 'answered';
+        }
+
+        return 'unanswered';
+
+
+
+
+    }
+
 
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,15 +10,25 @@
                 <div class="card-body">
                    @foreach ($questions as $question)
 
-                   <div class="media">
-
-                        <h3 class="mb-2 mt-1 font-bold text-lg text-blue-600"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                   <div class="d-flex mb-3">
+                    <div class="flex-col counters">
+                        <div class="vote">
+                            <strong>{{ $question->votes }}</strong> {{ str_plural('vote',$question->votes) }}
+                        </div>
+                        <div class="status {{ $question->status }}">
+                            <strong>{{ $question->answers }}</strong> {{ str_plural('answer',$question->answers) }}
+                        </div>
+                        <div class="view">
+                            {{ $question->views . " " . str_plural('view',$question->views) }}
+                        </div>
+                    </div>
+                        <div class="media-body">
+                            <h3 class="mb-2 font-bold text-lg text-blue-600"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                         <p class="lead">
                             Asked by
                             <a href="{{ $question->user->url }}" class="text-blue-500">{{ $question->user->name }}</a>
                             <small class="text-muted">{{ $question->created_date }}</small>
                         </p>
-                        <div class="media-body">
                         {{ str_limit($question->body, 250) }}
                         <hr>
                     </div>
