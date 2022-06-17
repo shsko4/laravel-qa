1981b3dd823e6c52f3b146fbfc15af78e557b2f2
Hussain Alhag <shsko4@gmail.com>
2022-06-17 22:02:06 +0200

    bulit question form

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -25,14 +25,30 @@
                 <div class="card-body">
 
                     <form action="{{ route('questions.store') }}" method="POST">
+                        @csrf
 
                         <div class="form-group">
                             <label for="question-title" class="m-2">Quetion Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control">
+                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}">
+
+                            @if ($errors->has('title'))
+
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('title') }}</strong>
+                            </div>
+
+                            @endif
                         </div>
                         <div class="form-group">
                             <label for="question-body" class="m-2">Explain your Question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}"></textarea>
+                            @if ($errors->has('body'))
+
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('body') }}</strong>
+                            </div>
+
+                            @endif
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-outline-primary btn-lg m-2">Ask this Question</button>
